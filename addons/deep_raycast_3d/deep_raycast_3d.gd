dbc574ecb339af8f6d733f8f1ca30ae5c8a6a577
Saulo-de-Souza <saulodevbr@gmail.com>
2025-11-05 15:26:13 -0300

    excludes

--- addons/deep_raycast_3d/deep_raycast_3d.gd
+++ addons/deep_raycast_3d/deep_raycast_3d.gd
@@ -21,28 +21,20 @@ signal cast_collider(results: Array[DeepRaycast3DResult])
 @export var enabled: bool = true
 @export_range(0.01, 1.0, 0.01, "or_greater", "suffix:m") var margin: float = 0.01
 @export_range(1, 32, 1) var max_results: int = 10
-@export_color_no_alpha() var color: Color = Color.WHITE
-@export_range(0.01, 1.0, 0.01) var opacity: float = 0.5
-@export var activate_emission: bool = false
-@export_range(0.0, 10.0, 0.01, "or_greater") var emission_energy: float = 2.0
-@export_range(0.01, 0.5, 0.01, "suffix:m") var radius: float = 0.05
+@export_color_no_alpha() var color: Color = Color.RED
+@export_range(0.01, 1.0, 0.01) var opacity: float = 0.7
+@export var activate_emission: bool = true
+@export_range(0.0, 10.0, 0.01, "or_greater") var emission_energy: float = 10.0
+@export_range(0.01, 0.5, 0.01, "suffix:m") var radius: float = 0.02
 @export var collide_with_bodies: bool = true
 @export var collide_with_areas: bool = false
 @export var hit_back_faces: bool = true
 @export var hit_from_inside: bool = true
-@export var excludes: Array[Node3D] = []:
+@export var excludes: Array[Node3D] = []
+@export var to: Node3D:
 	set(value):
-		excludes = value
-		_excludes.clear()
-
-		if Engine.is_editor_hint():
-			return
-
-		if excludes:
-			for e in excludes:
-				if e:
-					_excludes.append(e.get_rid())
-@export var to: Vector3 = Vector3.FORWARD * 10.0
+		to = value
+		update_configuration_warnings()
 @export_flags_3d_physics() var collision_mask = (1 << 0)
 @export_flags_3d_render() var layers = (1 << 0):
 	set(value):
@@ -78,10 +70,25 @@ func _create_line() -> void:
 
 	_node_container.add_child(_mesh_instance)
 	
+func _desative_raycast() -> void:
+	if not get_parent() is Node3D or to == null or get_parent() == to:
+		_mesh_instance.visible = false
+	else:
+		_mesh_instance.visible = true
+		
 
 func _update_line() -> void:
-	_distance = global_position.distance_to(to)
-	_direction = global_position.direction_to(to)
+	if to == null:
+		return
+	
+	if not get_parent() is Node3D:
+		return
+		
+	if get_parent() == to:
+		return
+		
+	_distance = global_position.distance_to(to.global_position)
+	_direction = global_position.direction_to(to.global_position)
 	_mesh.height = _distance
 	_mesh_instance.position.z = _distance / -2
 	_node_container.global_transform.origin = global_position
@@ -101,11 +108,20 @@ func _update_raycast() -> void:
 		return
 	if not enabled:
 		return
-
+		
+	if not get_parent() is Node3D:
+		return
+		
+	if to == null:
+		return
+		
+	if get_parent() == to:
+		return
+				
 	var space_state := get_world_3d().direct_space_state
 	var from := global_position
-	var to_dir := (to - global_position).normalized()
-	var remaining_distance := global_position.distance_to(to)
+	var to_dir := (to.global_position - global_position).normalized()
+	var remaining_distance := global_position.distance_to(to.global_position)
 
 	var local_excludes: Array = _excludes.duplicate()
 	deep_results.clear()
@@ -136,7 +152,7 @@ func _update_raycast() -> void:
 		local_excludes.append(hit["collider"].get_rid())
 
 		from = hit["position"] + to_dir * margin
-		remaining_distance = to.distance_to(from)
+		remaining_distance = to.global_position.distance_to(from)
 
 	if deep_results.size() > 0:
 		cast_collider.emit(deep_results)
@@ -146,6 +162,23 @@ func _update_raycast() -> void:
 
 
 #region Lifecycles =================================================================================
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings = []
+	if to == null:
+		warnings.append("The TO property in the inspector cannot be null.")
+		
+	if not get_parent() is Node3D:
+		warnings.append("The parent node of DeepRayCast3D must be a 3D node.")
+		
+	if get_parent() == to:
+		warnings.append("The TO property cannot be the parent node of DeepRayCast3D.")
+	return warnings
+	
+	
+func _enter_tree() -> void:
+	update_configuration_warnings()
+	
+	
 func _ready() -> void:
 	_material = RESOURCE_MATERIAL
 	if excludes:
@@ -155,9 +188,11 @@ func _ready() -> void:
 	_create_line()
 	_update_line()
 	_update_raycast()
+	_desative_raycast()
 
 
 func _physics_process(_delta: float) -> void:
 	_update_line()
 	_update_raycast()
+	_desative_raycast()
 #endregion Lifecycles ==============================================================================
